37f43307a2563fda5f78cdb8a8f0482dcfeb56b4
hira0taka <asl125asl74asl415@gmail.com>
2021-09-08 15:49:55 +0900

    quizgame

---
+++ src/Ans.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center border border-info my-1 px-2 ">
+    <p v-on:click="doAction">{{ ans }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ans",
+  props: {
+    ans: String,
+  },
+  methods: {
+      doAction() {
+          // thisは「このコンポーネントにある」
+         if(this.ans === "WHITE"){
+             console.log("正解")
+            //  「this.ans = "正解"」はエラー。propsの値を直接変更はできない。
+         } else {
+             console.log("不正解")
+         }
+      }
+  }
+};
+</script>
---
+++ src/Que.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alert alert-info">
+    <h2>{{ title }}</h2>
+    <p>{{ que }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Que",
+  props: {
+    title: String,
+    que: String,
+  }
+};
+</script>
