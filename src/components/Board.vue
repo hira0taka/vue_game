898701a58fd13b54866907b1119e6b09b4a05d1c
hira0taka <asl125asl74asl415@gmail.com>
2021-09-24 09:38:39 +0900

    hanteihairetu

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="board" v-on:click="checkAction">
-    <Cell
-      v-bind:count="count"
-      v-for="(_, id) in cells"
-      v-bind:key="id"
-      v-bind:number="id"
-      v-on:count-event="doCountUp"
-    />
+  <div>
+    <div class="board" v-on:click="checkAction">
+      <Cell
+        v-bind:count="count"
+        v-for="(_, id) in cells"
+        v-bind:key="id"
+        v-bind:number="id"
+        v-on:count-event="doCountUp"
+      />
+    </div>
+    <p class="message">{{ message }}</p>
   </div>
 </template>
 
@@ -22,26 +25,48 @@ export default {
     return {
       cells: [0, 0, 0, 0, 0, 0, 0, 0, 0],
       count: 0,
+      message: "",
     };
   },
-    methods: {
-      checkAction() {
-const columnA = [cells[0],cells[3],cells[6]]
-const columnB = [cells[1],cells[4],cells[7]]
-const columnC = [cells[2],cells[5],cells[8]]
+  methods: {
+    makeArrays(array) {
+      const columnA = [array[0], array[3], array[6]];
+      const columnB = [array[1], array[4], array[7]];
+      const columnC = [array[2], array[5], array[8]];
 
-const rowA = [cells[0],cells[1],cells[2]]
-const rowB = [cells[3],cells[4],cells[5]]
-const rowC = [cells[6],cells[7],cells[8]]
+      const rowA = [array[0], array[1], array[2]];
+      const rowB = [array[3], array[4], array[5]];
+      const rowC = [array[6], array[7], array[8]];
 
-const diagonalA = [cells[0],cells[4],cells[8]]
-const diagonalB = [cells[2],cells[14,cells[6]]
-      },
-doCountUp(clickNumber,cell) {
+      const diagonalA = [array[0], array[4], array[8]];
+      const diagonalB = [array[2], array[4], array[6]];
+
+      return [
+        columnA,
+        columnB,
+        columnC,
+        rowA,
+        rowB,
+        rowC,
+        diagonalA,
+        diagonalB,
+      ];
+    },
+    checkWin(array){
+      if(array.every(id => id === "〇")){
+        this.message = "Aの勝ち！"
+      } else if (array.every(id => id === "×")){
+        this.message = "Bの勝ち！"
+      }
+    },
+    doCountUp(clickNumber, cell) {
       this.count++;
-      this.cells[clickNumber] = cell;
+      this.cells[clickNumber - 1] = cell;
+
+      const checkArrays = this.makeArrays(this.cells);
+      checkArrays.map((array) => this.checkWin(array))
     },
-    },
+  },
 };
 </script>
 
